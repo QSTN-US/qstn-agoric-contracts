563426b0866091ca8bdc4068ba2372d3630ee9f0
JoE11-y <Josephedoh77@gmail.com>
2025-12-08 03:01:23 +0100

    feat: cleanup contract deploy

---
+++ deploy/src/qstn.contract.permit.js
@@ -0,0 +1,25 @@
+// TODO: move meta.name
+import { orchPermit } from '../tools/orch.start.js';
+
+export const name = /** @type {const} */ 'qstn';
+
+/**
+ * @import { BootstrapManifestPermit } from '@agoric/vats/src/core/lib-boot.js';
+ */
+
+/**
+ * @satisfies {BootstrapManifestPermit}
+ */
+export const permit = /** @type {const} */ {
+  produce: { [/** @type {const} */ `${name}Kit`]: true },
+  consume: {
+    ...orchPermit,
+    chainInfoPublished: true,
+    [/** @type {const} */ `${name}Kit`]: true,
+  },
+  instance: { produce: { [name]: true } },
+  installation: { consume: { [name]: true } },
+  brand: {},
+  issuer: { consume: { BLD: true, IST: true } },
+};
+harden(permit);
